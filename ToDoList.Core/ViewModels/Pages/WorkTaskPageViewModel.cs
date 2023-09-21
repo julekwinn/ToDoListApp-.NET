7cf4fd5454d3c40a85b2a03f9970337bb76cd4c6
Juliusz <julek.winn@gmail.com>
2023-09-21 12:01:07 +0200

    Add void "AddNewTask"

--- ToDoList.Core/ViewModels/Pages/WorkTaskPageViewModel.cs
+++ ToDoList.Core/ViewModels/Pages/WorkTaskPageViewModel.cs
@@ -3,5 +3,19 @@
 public class WorkTaskPageViewModel
 {
 
+    public List<WorkTaskViewModel> WorkTaskList { get; set; }
 
+    public string NewWorkTaskTitle { get; set; }
+
+    public string NewWorkTaskDescription { get; set; }
+
+    private void AddNewTask()
+    {
+        var newTask = new WorkTaskViewModel
+        {
+            Title = NewWorkTaskTitle,
+            Description = NewWorkTaskDescription,
+            CreatedDate = DateTime.Now
+        };
+    }
 }
